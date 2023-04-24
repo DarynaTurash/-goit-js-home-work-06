90ed1b5b70cb32ede5b95b631073348d2e87c20a
DarynaTurash <daryna.turash@gmail.com>
2023-04-24 18:58:44 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -7,15 +7,42 @@ const containerOfControls = document.querySelector("#controls");
 const input = containerOfControls.firstElementChild;
 const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
+const divForBoxes = document.querySelector('#boxes');
 
 
-btnCreate.addEventListener("input", onCreateCollection);
+input.addEventListener('change', (event) => {
+  let amountValue = 0;
 
-function onCreateCollection(event) {
-  const newDiv = document.createElement("div");
-  newDiv.width = "30px";
-  newDiv.height = "30px";
+  if (Number(event.currentTarget.value) >= Number(event.currentTarget.min) && Number(event.currentTarget.value) <= Number(event.currentTarget.max)){
+    amountValue = Number(event.currentTarget.value);
+  btnCreate.addEventListener('click', createBoxes(amountValue));
+  }
+  else {
+    alert('Ввели невірне число!')
+  }
+});
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  for(let i = 0; i < amount; i += Number(input.step)){
+    const boxEl = document.createElement('div');
+    boxEl.style.backgroundColor = getRandomHexColor(),
+    boxEl.style.width = 30 + i * 10 + 'px';
+    boxEl.style.height = 30 + i * 10 + 'px';
+    boxEl.classList.add('box')
+    divForBoxes.append(boxEl);
+  }
+}
+
+function destroyBoxes() {
+  divForBoxes.innerHTML = '';
+  input.value = '';
 };
+
+
+
+
 
 
 
